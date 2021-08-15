d813ddf37e78396f8748cc3e1e94cc531af04dd6
antelor <ramiro.antelo4@gmail.com>
2021-08-15 19:17:11 -0300

    movi las querys a firestore a componentdidmount para evitar delay al seleccionar pj

--- src/App.js
+++ src/App.js
@@ -14,8 +14,6 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
-const charList = ['waldo', 'mage'];
-
 class App extends Component {
   constructor(props) {
     super(props);
@@ -23,13 +21,15 @@ class App extends Component {
     this.state = {
       score: 0,
       endScore: 0,
-      chars: charList,
+      chars: [],
     };
 
+    this.charListInit = this.charListInit.bind(this);
     this.checkSelection = this.checkSelection.bind(this);
     this.removeChar=this.removeChar.bind(this);
   }
 
+  //mostrar o esconder menu
   toggleMenu(e) {
     let dropMenu = document.querySelector('.dropMenu');
     
@@ -38,15 +38,18 @@ class App extends Component {
     dropMenu.style.top = (e.clientY - 20) + 'px';
   }
 
+  //eliminar personaje de charList
   removeChar(name) {
     let newState = this.state;
+
     newState.chars = newState.chars.filter(function (item) {
-      return item !== name;
+      return item.name != name;
     });
     
     this.setState(newState);
   }
 
+  //chequea si ganaste y muestra banner
   checkWin() {
     //si no quedan personajes, ganaste
     if (this.state.chars.length === 0) {
@@ -55,34 +58,30 @@ class App extends Component {
       winDiv.classList.add('winDiv');
       winDiv.textContent = 'Ganaste!!!!';
       document.querySelector('[id=root]').appendChild(winDiv);       
-
       //stop contador de score
       clearInterval(this.interval);
     }
   }
   
-  async checkSelection(e) {
-    
-    let charDocument = await firebase.firestore()
-    .collection('charPositions')
-    .doc(e.target.name)
-    .get();
-    
-    let charPositions = charDocument._delegate._document.data.value.mapValue.fields;
-    
-    //separo coordenadas en objeto para hacer mas facil la lectura
-    let coord = {
-      x0: charPositions.x0.integerValue,
-      x1: charPositions.x1.integerValue,
-      y0: charPositions.y0.integerValue,
-      y1: charPositions.y1.integerValue
+  //check si la seleccion abarca personaje
+  checkSelection(e) {
+    let x0, x1, y0, y1 = -1;
+
+    //encontrar coordeandas de personaje
+    for(let i = 0; i<this.state.chars.length; i++){
+      if(this.state.chars[i].name === e.target.name){
+        x0 = this.state.chars[i].x0;
+        x1 = this.state.chars[i].x1;
+        y0 = this.state.chars[i].y0;
+        y1 = this.state.chars[i].y1;
+      }
     }
-    
-    //chequeo de si la seleccion abarca el personaje
-    if (e.clientX >= coord.x0 &&
-      e.clientX <= coord.x1 &&
-      e.clientY >= coord.y0 &&
-      e.clientY <= coord.y1)
+
+    //chequeo de coordenadas
+    if (e.clientX >= x0 &&
+      e.clientX <= x1 &&
+      e.clientY >= y0 &&
+      e.clientY <= y1)
     {
         //div yes
         const yesDiv = document.createElement('div');
@@ -113,6 +112,33 @@ class App extends Component {
       let currScore = this.state.score;
       this.setState({ score: currScore + 1 })
     }, 1000);
+
+    this.charListInit();
+  }
+
+  async charListInit() {
+    let newState = this.state;
+
+    //query
+    let charDocument = await firebase.firestore()
+    .collection('charPositions')
+    .get();
+    
+    //pasar a array
+    let charArray = charDocument.docs.map(doc => doc.data());
+
+    //por cada entrada agrego a charList
+    charArray.forEach((item) => {
+      newState.chars.push({
+        name: item.name,
+        x0: item.x0,
+        x1: item.x1,
+        y0: item.y0,
+        y1: item.y1
+      });
+    });
+
+    this.setState(newState);
   }
 
   componentWillUnmount() {
